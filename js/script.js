804a13f70f0918b462e0498ae724b5bdd7fb2a12
Neuyer <39165369+Neuyer@users.noreply.github.com>
2018-05-25 04:35:14 -0300

    muita coisa mudada

--- js/script.js
+++ js/script.js
@@ -1,20 +1,88 @@
-//Script
-var slideIndex = 0;
-showSlides();
+//	<script>
+      // script cardápio coluna 1
+        $(".pratosL .a").click(function(){
+        var t = $(".pratosL .a").text();
+        $("#destaqueprato").html(t);
+        var bg = $(".pratosL .a").attr("src");
+        $("#destaque").css({"background-image":"url("+bg+")","background-position":"center"}).delay(500).slideDown();
+        $(".pratosC,.pratosR").fadeOut("fast");
+        });
+          $(".fecha").click(function(){
+          $("#destaque").slideUp();
+          $(".pratosC,.pratosR").delay(400).fadeIn("slow");
 
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    for (i = 0; i < slides.length; i++) {
-       slides[i].style.display = "none";  
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}    
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 3500); // Change image every 3.5 seconds
-}
+        });
+
+        $(".pratosL .b").click(function(){
+        var t = $(".pratosL .b").text();
+        $("#destaqueprato").html(t);
+        var bg = $(".pratosL .b").attr("src");
+        $("#destaque").css({"background-image":"url("+bg+")","background-position":"center"}).delay(500).slideDown();
+        $(".pratosC,.pratosR").fadeOut("fast");
+        });
+          $(".fecha").click(function(){
+          $("#destaque").slideUp();
+          $(".pratosC,.pratosR").delay(400).fadeIn("slow");
+        });
+
+      // script cardápio coluna 2
+
+
+        $(".pratosC .a").click(function(){
+        var t = $(".pratosC .a").text();
+        $("#destaqueprato").html(t);
+          var bg = $(".pratosC .a").attr("src");
+          $("#destaque").css({"background-image":"url("+bg+")","background-position":"center"}).delay(800).slideDown();
+          $(".pratosL,.pratosR").fadeOut("fast");
+
+          });
+            $(".fecha").click(function(){
+            $("#destaque").slideUp();
+            $(".pratosL,.pratosR").delay(400).fadeIn("slow");
+
+          });
+
+          $(".pratosC .b").click(function(){
+          var t = $(".pratosC .b").text();
+          $("#destaqueprato").html(t);
+          var bg = $(".pratosC .b").attr("src");
+          $("#destaque").css({"background-image":"url("+bg+")","background-position":"center"}).delay(800).slideDown();
+          $(".pratosL,.pratosR").fadeOut("fast");
+
+          });
+            $(".fecha").click(function(){
+            $("#destaque").slideUp();
+            $(".pratosL,.pratosR").delay(400).fadeIn("slow");
+
+          });
+
+        // script cardápio coluna 3
+
+
+        $(".pratosR .a").click(function(){
+        var t = $(".pratosR .a").text();
+        $("#destaqueprato").html(t);
+          var bg = $(".pratosR .a").attr("src");
+          $("#destaque").css("background-image","url("+bg+")").delay(800).slideDown();
+          $(".pratosL,.pratosC").fadeOut("fast");
+
+          });
+            $(".fecha").click(function(){
+            $("#destaque").slideUp();
+            $(".pratosC,.pratosL").delay(400).fadeIn("slow");
+
+          });
+
+          $(".pratosR .b").click(function(){
+          var t = $(".pratosR .b").text();
+          $("#destaqueprato").html(t);
+          var bg = $(".pratosR .b").attr("src");
+          $("#destaque").css("background-image","url("+bg+")").delay(800).slideDown();
+          $(".pratosC,.pratosL").fadeOut("fast");
+
+          });
+            $(".fecha").click(function(){
+            $("#destaque").slideUp();
+            $(".pratosL,.pratosC").delay(400).fadeIn("slow");
+
+          });
