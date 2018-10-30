994019f2df6fab52d68f77fb08ee9353af15c486
Neuyer <39165369+Neuyer@users.noreply.github.com>
2018-10-30 17:07:09 -0300

    mapa e instaFeed
    added location and instafeed.js library

--- js/newJs.js
+++ js/newJs.js
@@ -8,4 +8,17 @@ function main() {
       $("section").slideUp();
       $("#"+section+id).slideDown();
     });
+
+    /**var feed = new Instafeed({
+       get: 'tagged',
+       tagName: 'awesome',
+       clientId: 'YOUR_CLIENT_ID'
+   });
+   feed.run();*/
+    var userFeed = new Instafeed({
+           get: 'user',
+           userId: '1524641572',
+           accessToken: '1524641572.1677ed0.a7899e6798e54ef2bdc161b26e449615'
+       });
+       userFeed.run();
 }
