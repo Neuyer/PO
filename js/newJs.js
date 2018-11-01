b15d730108b55392d317c3f300aa5257d875d93b
Neuyer <39165369+Neuyer@users.noreply.github.com>
2018-11-01 15:53:36 -0300

    Responsividade
    implementação do menu sandwich
    ajuste das sections

--- js/newJs.js
+++ js/newJs.js
@@ -6,19 +6,32 @@ function main() {
       var id = $(this).attr('id');
       var section="section"
       $("section").slideUp();
-      $("#"+section+id).slideDown();
+      $("#"+section+id).css("background-image","url(images/bg"+id+".png)").slideDown();
     });
 
+  function nav() {                          //funcao q transforma a av em sandwich
+    if($(window).width()<= 414){
+      $("button").click(function(event) {
+        $("nav").toggle(function(){$(this).animate({height: "100vh", width:"80%"}, 400)})
+      });
+      $("li").click(function(event) {
+        $("nav").toggle(function(){$(this).animate({height: "100vh", width:"80%"}, 400)})
+      });
+  }
+}
+
+nav();
     /**var feed = new Instafeed({
-       get: 'tagged',
-       tagName: 'awesome',
-       clientId: 'YOUR_CLIENT_ID'
+        get: "popular",
+       clientId: '1368612662'
    });
    feed.run();*/
     var userFeed = new Instafeed({
            get: 'user',
            userId: '1524641572',
-           accessToken: '1524641572.1677ed0.a7899e6798e54ef2bdc161b26e449615'
+           accessToken: '1524641572.1677ed0.a7899e6798e54ef2bdc161b26e449615',
+           limit: "18",
+           sortBy: "most-recent"
        });
        userFeed.run();
 }
